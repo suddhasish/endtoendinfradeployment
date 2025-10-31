92fdde26ffc13717bf2eb795d687894013e2ecb7
Suddhasish kar <suddhasiishkar@gmail.com>
2025-10-31 11:38:54 +0530

    fix: remove duplicate outputs from main.tf files

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -396,18 +396,3 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   allow_virtual_network_access = true
   allow_gateway_transit        = false
 }
-
-output "hub_rg_name" { value = azurerm_resource_group.hub.name }
-output "hub_vnet_id" { value = azurerm_virtual_network.hub.id }
-output "appgw_subnet_id" { value = azurerm_subnet.appgw.id }
-output "pe_subnet_id" { value = azurerm_subnet.pe.id }
-output "bastion_subnet_id" { value = azurerm_subnet.bastion.id }
-output "firewall_subnet_id" { value = azurerm_subnet.firewall.id }
-output "spoke_rg" { value = azurerm_resource_group.spoke_rg }
-output "spoke_aks_subnet_ids" { value = { for k, s in azurerm_subnet.aks : k => s.id } }
-output "spoke_db_subnet_ids" { value = { for k, s in azurerm_subnet.db : k => s.id } }
-output "private_dns_zone_keyvault_id" { value = azurerm_private_dns_zone.keyvault.id }
-output "private_dns_zone_storage_blob_id" { value = azurerm_private_dns_zone.storage_blob.id }
-output "private_dns_zone_storage_file_id" { value = azurerm_private_dns_zone.storage_file.id }
-output "private_dns_zone_sql_id" { value = azurerm_private_dns_zone.sql.id }
-output "private_dns_zone_aks_id" { value = azurerm_private_dns_zone.aks.id }
